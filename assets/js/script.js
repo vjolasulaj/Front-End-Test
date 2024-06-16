af80cc3e9f98fdaf3049d138402a108ff57a5f0b
Vjola Sulaj <sulaj.vjolaa@gmail.com>
2024-06-16 09:29:57 +0200

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+var swiper = new Swiper(".swiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    slidesPerGroup: 1,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      }
+    }
+});
+
+
